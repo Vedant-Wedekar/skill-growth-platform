f169efa7df56df17d487cb314e6ae83c52bb8a10
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-02-17 17:07:09 +0530

    react app for dev

---
+++ my-app/src/component/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+       <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1> 
+  <div className="App bg-red-500 w-full h-screen"></div>
+    </div>
+  )
+}
+
+export default header
