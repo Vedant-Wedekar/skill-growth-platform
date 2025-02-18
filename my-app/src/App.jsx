04929f24f221bb9c0e956ce98c3fc1cb7491d1f9
VEDANT WEDEKAR <vedantwedekar@gmail.com>
2025-02-18 09:25:39 +0000

    idk

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Header from './component/header'
+import 'google-fonts/css2?family=Roboto:wght@400;700&display=swap';
+
 const App = () => {
   return (
     <div>
--- my-app/src/component/header.jsx
+++ my-app/src/component/header.jsx
@@ -2,11 +2,27 @@ import React from 'react'
 
 const header = () => {
   return (
-    <div>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1> 
-  <div className="App bg-red-500 w-full h-screen"></div>
+    <div className="w-full h-[200px] mt-3">
+
+      <div className="header flex justify-around items-center">
+        <div className="logo mr-[150px]">
+          <img src=".png" alt="Logo" />
+        </div>
+
+        <div className="hn flex justify-around  w-[180px] h-[30px] rounded-3xl bg-gray-200">
+          <div className="io">Home</div>
+          <div className="io">New Course</div>
+        </div>
+
+        <div className="hcma flex justify-around w-[400px] mr-[-80px]">
+          <div className="w-[80px] h-[30px] flex justify-center items-center bg-gray-100 rounded-3xl border-black border-[1px] font-Roboto">Home</div>
+          <div className="w-[80px] h-[30px] flex justify-center items-center bg-gray-100 rounded-3xl border-black border-[1px]">Course</div>
+          <div className="w-[100px] h-[30px] flex justify-center items-center bg-gray-100 rounded-3xl border-black border-[1px]">Mentorship</div>
+          <div className="w-[80px] h-[30px] flex justify-center items-center bg-gray-100 rounded-3xl border-black border-[1px]">Help</div>
+
+        </div>
+      </div>
+     
     </div>
   )
 }
